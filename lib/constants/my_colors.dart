41f3625278e212d8635fb5a2528cbd966a9580b4
Milad <milad.kh4lighi@gmail.com>
2023-02-12 13:00:00 +0330

    init project and add custom colors

---
+++ lib/constants/my_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/animation.dart';
+
+class MyColors {
+  MyColors._();
+
+  static const primaryColor = Color(0xFFFFC045);
+  static const bgColor = Color.fromARGB(255, 29, 29, 29);
+  static const primaryTextColor = Color(0xFFFFFFFF);
+  static const secondaryTextColor = Color(0xFFBABABA);
+  static const secondaryColor = Color.fromARGB(255, 24, 24, 24);
+  static const cardBgColor = Color(0xFF2C2C2C);
+}
