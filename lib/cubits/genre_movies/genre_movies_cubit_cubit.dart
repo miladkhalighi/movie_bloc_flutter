4a7b8bf9a4000113ae9eb6d7dd9c1e0ffdfbab42
Milad <milad.kh4lighi.com>
2023-02-28 14:19:42 +0330

    create genre movie state and cubit

---
+++ lib/cubits/genre_movies/genre_movies_cubit_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_movie/models/genre.dart';
+import 'package:flutter_movie/repository/movie_repository.dart';
+
+part 'genre_movies_cubit_state.dart';
+
+class GenreMoviesCubit extends Cubit<GenreMoviesState> {
+  final MovieRepository movieRepository;
+  GenreMoviesCubit({required this.movieRepository})
+      : super(GenreMoviesState.initial());
+
+  Future<void> fetchGenres() async {
+    emit(state.copyWith(status: GenreStatus.initial));
+    try {
+      emit(state.copyWith(status: GenreStatus.loading));
+      final List<Genre> genres = await movieRepository.fetchGenres();
+      emit(state.copyWith(status: GenreStatus.loaded,genres: genres));
+    } catch (e) {
+      emit(state.copyWith(status: GenreStatus.error));
+    }
+  }
+}
