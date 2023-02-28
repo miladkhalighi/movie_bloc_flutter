018b86d4d19d575d82fede9c951eeea8fd76cb12
Milad <milad.kh4lighi.com>
2023-02-28 14:22:18 +0330

    create up comming movies state and cubit

---
+++ lib/cubits/up_comming_movies/up_comming_movies_cubit.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_movie/cubits/genre_movies/genre_movies_cubit_cubit.dart';
+
+import 'package:flutter_movie/models/movie.dart';
+import 'package:flutter_movie/repository/movie_repository.dart';
+
+part 'up_comming_movies_state.dart';
+
+class UpCommingMoviesCubit extends Cubit<UpCommingMoviesState> {
+  final MovieRepository repository;
+  final GenreMoviesCubit genreMoviesCubit;
+  late final StreamSubscription streamSubscription;
+  UpCommingMoviesCubit({
+    required this.repository,
+    required this.genreMoviesCubit,
+  }) : super(UpCommingMoviesState.intial()) {
+    streamSubscription =
+        genreMoviesCubit.stream.listen((GenreMoviesState genreState) async {
+      //Genre list must not be empty , becuase movies have to convert genres id to string
+      if (genreState.status == GenreStatus.loaded &&
+          genreState.genres.isEmpty) {
+        emit(state.copyWith(status: UpCommingStatus.error));
+      }
+    });
+  }
+
+  Future<void> fetchUpCommingMovies() async {
+    emit(state.copyWith(status: UpCommingStatus.loading));
+    try {
+      streamSubscription =
+          genreMoviesCubit.stream.listen((GenreMoviesState genreState) async {
+        if (genreState.status == GenreStatus.loaded &&
+            genreState.genres.isNotEmpty) {
+          final List<Movie> movies = await repository.fetchUpCommingMovies();
+          emit(state.copyWith(status: UpCommingStatus.loaded, movies: movies));
+        } else {
+          emit(state.copyWith(status: UpCommingStatus.error));
+        }
+      });
+    } catch (e) {
+      emit(state.copyWith(status: UpCommingStatus.error));
+    }
+  }
+}
