664cf5d913ae58408c0e0216393e3001258e8c4a
Milad <milad.kh4lighi@gmail.com>
2023-02-28 15:01:16 +0330

    update home screen ui and logics

--- lib/constants/my_text_styles.dart
+++ lib/constants/my_text_styles.dart
@@ -8,4 +8,9 @@ class MyTextStyles {
   static var title = GoogleFonts.aclonica(fontSize: 16, color: MyColors.primaryTextColor);
   static var subTitle = GoogleFonts.goldman(fontSize: 12,color: MyColors.secondaryTextColor);
 
+  static var titleSecondary = GoogleFonts.donegalOne(fontSize: 24, color: MyColors.primaryTextColor);
+  static var subTitleSecondary = GoogleFonts.goldman(fontSize: 16,color: MyColors.secondaryTextColor);
+
+  static var body = GoogleFonts.goldman(fontSize: 14,color: MyColors.primaryTextColor);
+
 }
