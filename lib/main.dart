a7635f8f84a0da180c4766b89d527250eacfc436
Milad <milad.kh4lighi.com>
2023-03-01 14:12:25 +0330

    add photos cubit in UI

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_movie/cubits/cast_movies/cast_movies_cubit.dart';
 import 'package:flutter_movie/cubits/crew_movies/crew_movies_cubit.dart';
 import 'package:flutter_movie/cubits/genre_movies/genre_movies_cubit_cubit.dart';
 import 'package:flutter_movie/cubits/most_popular_movies/most_popular_movies_cubit_cubit.dart';
+import 'package:flutter_movie/cubits/photos_movie/photos_movie_cubit.dart';
 import 'package:flutter_movie/cubits/top_rated_movies/top_rated_movies_cubit.dart';
 import 'package:flutter_movie/cubits/up_comming_movies/up_comming_movies_cubit.dart';
 import 'package:flutter_movie/models/movie.dart';
@@ -58,6 +59,10 @@ class MyApp extends StatelessWidget {
             create: (context) =>
                 CrewMoviesCubit(repository: context.read<MovieRepository>()),
           ),
+          BlocProvider(
+            create: (context) =>
+                PhotosMovieCubit(repository: context.read<MovieRepository>()),
+          )
         ],
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
