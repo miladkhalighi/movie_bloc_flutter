1123ff663a2c88743055eb72b63f290884730596
Milad <milad.kh4lighi.com>
2023-03-01 14:11:26 +0330

    create photos movie states and cubit

---
+++ lib/cubits/photos_movie/photos_movie_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_movie/repository/movie_repository.dart';
+
+part 'photos_movie_state.dart';
+
+class PhotosMovieCubit extends Cubit<PhotosMoviesStates> {
+  final MovieRepository repository;
+  PhotosMovieCubit({required this.repository})
+      : super(PhotosMoviesStates.initial());
+
+  Future<void> fetchMoviePhotos(int movieId) async {
+    try {
+      emit(state.copyWith(status: PhotosStatus.loading));
+      final photos = await repository.fetchMoviePhotos(movieId);
+      emit(state.copyWith(status: PhotosStatus.loaded,photos: photos));
+    } catch (e) {
+      emit(state.copyWith(status: PhotosStatus.error));
+    }
+  }
+}
