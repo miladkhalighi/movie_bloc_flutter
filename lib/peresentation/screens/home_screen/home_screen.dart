dda1a89b86a09b3857b608c65ad6db72313d75ca
Milad <milad.kh4lighi@gmail.com>
2023-03-05 16:47:00 +0330

    add most popular list widget

--- lib/peresentation/screens/home_screen/home_screen.dart
+++ lib/peresentation/screens/home_screen/home_screen.dart
@@ -8,6 +8,7 @@ import 'package:flutter_movie/logic/cubits/most_popular_movies/most_popular_movi
 import 'package:flutter_movie/logic/cubits/top_rated_movies/top_rated_movies_cubit.dart';
 import 'package:flutter_movie/logic/cubits/up_comming_movies/up_comming_movies_cubit.dart';
 import 'package:flutter_movie/peresentation/screens/details_screen/details_screen.dart';
+import 'package:flutter_movie/peresentation/screens/home_screen/components/most_popular_list.dart';
 import 'package:flutter_movie/peresentation/shared_widgets/movie_card_small.dart';
 import 'package:flutter_movie/peresentation/shared_widgets/most_popular_card.dart';
 import 'package:flutter_movie/peresentation/shared_widgets/title_with_text_btn.dart';
@@ -22,8 +23,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  bool loading = false;
-  int selectedMainCardItem = 0;
 
   @override
   void initState() {
@@ -34,11 +33,6 @@ class _HomeScreenState extends State<HomeScreen> {
     _fetchUpComming();
   }
 
-  setMainCardItem(int index) {
-    setState(() {
-      selectedMainCardItem = index;
-    });
-  }
 
   void _fetchGenres() async {
     context.read<GenreMoviesCubit>().fetchGenres();
@@ -74,7 +68,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     height: 24,
                   ),
                   TitleWithTextBtn(onPressed: () {}, title: "Most popular"),
-                  MostPopularSelection(context),
+                  const MostPopularList(),
                   const SizedBox(
                     height: 48,
                   ),
@@ -98,77 +92,6 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
           )),
-    );
-  }
-
-  Widget MostPopularSelection(BuildContext context) {
-    var size = MediaQuery.of(context).size;
-    return SizedBox(
-      height: size.height / 2.236,
-      child: BlocBuilder<MostPopularMoviesCubit, MostPoularMoviesState>(
-          builder: (context, state) {
-            if (state.status == MostPupularMoviesStatus.initial) {
-              return const SpinKitFadingFour(
-                color: MyColors.primaryColor,
-              );
-            } else if (state.status == MostPupularMoviesStatus.loading) {
-              return const SpinKitFadingFour(
-                color: MyColors.primaryColor,
-              );
-            } else if (state.status == MostPupularMoviesStatus.loaded) {
-              return CarouselSlider.builder(
-                //carouselController: carouselController,
-                itemCount: state.movies.length,
-                itemBuilder: ((context, index, realIndex) {
-                  var genreIds = state.movies[index].genreIds;
-                  String joinedString = getGenreNames(genreIds,
-                          context.read<GenreMoviesCubit>().state.genres)
-                      .join(', ');
-                  String heroTag = '${UniqueKey()}${state.movies[index].id}';
-
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: MostPopularCard(
-                      heroTag: heroTag,
-                      img: state.movies[index].posterUrl,
-                      title: state.movies[index].title,
-                      category: joinedString,
-                      rate: state.movies[index].voteAverage! / 2 ?? 0.0,
-                      selected: index == selectedMainCardItem ? true : false,
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => DetailsScreen(
-                                      movie: state.movies[index],
-                                      heroTag: heroTag,
-                                    )));
-                      },
-                    ),
-                  );
-                }),
-                options: CarouselOptions(
-                  height: size.height / 2.236,
-                  //aspectRatio: 16 / 9,
-                  viewportFraction: 0.6,
-                  initialPage: selectedMainCardItem,
-                  enableInfiniteScroll: true,
-                  reverse: false,
-                  enlargeCenterPage: true,
-                  enlargeFactor: 0.3,
-                  onPageChanged: (int index, _) {
-                    setMainCardItem(index);
-                  },
-                  scrollDirection: Axis.horizontal,
-                ),
-              );
-            } else if (state.status == MostPupularMoviesStatus.error) {
-              return const Text('ERROR');
-            } else {
-              return const SizedBox.shrink();
-            }
-          },
-          ),
     );
   }
 
@@ -292,3 +215,5 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
+
