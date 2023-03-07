b048d24c18e892c6577ffc46278e61d534a849d1
Milad <milad.kh4lighi@gmail.com>
2023-03-07 18:38:11 +0330

    create bottom navigation cubit logics

---
+++ lib/logic/cubits/bottom_navigation/bottom_navigation_cubit.dart
@@ -0,0 +1,12 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'bottom_navigation_state.dart';
+
+class BottomNavigationCubit extends Cubit<BottomNavigationState> {
+  BottomNavigationCubit() : super(BottomNavigationState.initial());
+
+  void setIndex(int index) {
+    emit(state.copyWith(currentIndex: index));
+  }
+}
